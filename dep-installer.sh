e27f44878d096eca4c494b0a40ed4001e39f60a1
Tomas <tomas.mella-pickersgill@warwick.ac.uk>
2022-03-05 11:30:09 +0000

    Isolating and parameterising docker envs for parallel execution

--- dep-installer.sh
+++ dep-installer.sh
@@ -12,6 +12,8 @@ mkdir -p $WORK_DIR/bin
 # shellcheck disable=SC2164
 rm -rf dep_build && mkdir "$BUILD_DIR" && cd "$BUILD_DIR"
 
+
+
 if [[ $OS_DIS == 'Darwin'* ]]; then
   echo 'Installing Geth and Tools for Mac ...'
   curl -sLO https://gethstore.blob.core.windows.net/builds/geth-alltools-darwin-amd64-1.10.15-8be800ff.tar.gz
